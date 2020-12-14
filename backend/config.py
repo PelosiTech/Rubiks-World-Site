e61400870bb26d4e8411cf3ad2929bc3f78cf6f1
Carlo Pelosi <cpelosi@asu.edu>
2020-12-13 21:38:21 -0700

    back to normal did the add on trick

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
   SECRET_KEY=os.environ.get('SECRET_KEY')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
-  SQLALCHEMY_DATABASE_URI="postgresql://rubiksuser:password@localhost/rubikstable"
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
   SQLALCHEMY_ECHO=True
