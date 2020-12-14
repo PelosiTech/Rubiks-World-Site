c19904a046099601d643e46176eb3abe2d65ceb1
Carlo Pelosi <cpelosi@asu.edu>
2020-12-13 20:02:36 -0700

    second attempt for fix on Docker

--- rubiksFlaskBackendAPI/__init__.py
+++ rubiksFlaskBackendAPI/__init__.py
@@ -39,8 +39,7 @@ def inject_csrf_token(response):
     response.set_cookie('csrf_token',
                         generate_csrf(),
                         secure=True if os.environ.get('FLASK_ENV') else False,
-                        samesite='Strict' if os.environ.get(
-                            'FLASK_ENV') else None,
+                        samesite='Strict' if os.environ.get('FLASK_ENV') else None,
                         httponly=True)
     return response
 
